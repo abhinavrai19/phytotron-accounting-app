030fc51921eecc190c85f01666d500341b408cfa
abhinavrai0 <saintabhinav@gmail.com>
2017-09-09 12:19:39 -0400

    finished Chamber functionality and views

--- public/chamber-add-page/client.chamber-add-page.component.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('phytotronAccountingApp')
-    .component('chamberAddPage',{
-        templateUrl: 'chamber-add-page/client.chamber-add-page.template',
-        controller: ['ChamberService', function ChamberAddPageController(ChamberService){
-            var ctrl = this;
-        }]
-    });
