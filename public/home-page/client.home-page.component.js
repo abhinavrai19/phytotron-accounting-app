f2c96f65e54fe140ffba8c1fe91a374bbd85949d
abhinavrai0 <saintabhinav@gmail.com>
2017-09-13 08:20:24 -0400

    Minor UI modifications and bug fixes

--- public/home-page/client.home-page.component.js
+++ public/home-page/client.home-page.component.js
@@ -3,5 +3,6 @@ angular.module('phytotronAccountingApp').
     component('homePage',{
         templateUrl: 'home-page/client.home-page.template.html',
         controller: function HomePageController(){
+
         }
 });
--- public/top-navigation-bar/client.top-navigation-bar.component.js
+++ public/top-navigation-bar/client.top-navigation-bar.component.js
@@ -3,9 +3,10 @@ angular.module('phytotronAccountingApp').
     component('topNavigationBar',{
         templateUrl: 'top-navigation-bar/client.top-navigation-bar.template.html',
         controller: function TopNavigationBarController($location){
+            var ctrl = this;
 
-            this.navigateTo = function(locationPath){
+            ctrl.navigateTo = function(locationPath){
                 $location.path(locationPath);
-            }
+            };
         }
 });
