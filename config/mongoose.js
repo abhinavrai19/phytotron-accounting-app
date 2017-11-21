f7442f586183f29845baa5885d769b94363b4454
abhinavrai0 <saintabhinav@gmail.com>
2017-11-21 15:25:29 -0500

    added MongoDB authentication

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,20 @@
 // Set up database connection
 var mongoose = require('mongoose');
+var constants = require('../constants');
 
 module.exports = function(){
     mongoose.Promise = global.Promise;
     mongoose.connect('mongodb://127.0.0.1/phytotron_accounts',{
-        useMongoClient: true
+        useMongoClient: true,
+        user: constants.MONGO_DB_USER_NAME,
+        pass: constants.MONGO_DB_USER_PASSWORD,
+        auth: {
+            authdb: constants.MONGO_DB_USER_ROLE
+        }
     });
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB Connection error'));
 
     return db;
-
 }
 
--- constants.js
+++ constants.js
@@ -7,13 +7,22 @@ module.exports = {
     // Path to connect to database
     MONGO_DB_PATH: 'mongodb://127.0.0.1/phytotron_accounting_database',
 
+    MONGO_DB_USER_NAME: 'joe',
+
+    MONGO_DB_USER_PASSWORD: 'phytomongo',
+
+    MONGO_DB_USER_ROLE: 'admin',
+
+    MONGO_DB_PORT: '27017',
+
+    // Phytotron ID (used in invoices)
     PHYTOTRON_ID: 'A020201',
 
     // Invoice Template path
     INVOICE_REPORT_TEMPLATE_PATH: __dirname+'/views/'+'invoice_report.hbs',
 
     // Directory path to store the invoice reports in
-    INVOICE_SAVE_DIRECTORY_PATH: '/Users/abhinavrai/Downloads/',
+    INVOICE_SAVE_DIRECTORY_PATH: '/Users/abhinavrai/Desktop/Phytotron/',
 
     // Decimals to round off to while doing invoice amount calculations
     ROUND_OFF_AMOUNT_TO_VALUE: 2
